e80d44f847244af9ed248278e221a908395c6c9d
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-16 13:40:07 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -14,5 +14,5 @@ function imageSelector (){
   return $('img[alt="cat sleeping"]');
 }
 function checkbocInputSelectors (){
-  return $('input[type="check box"]');
+  return $('input[type="checkbox"]');
 }
